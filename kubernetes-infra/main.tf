a74587b7dfe6c847a2b6efbe62a1aacaec2dc9fa
Bernardo Pires Mesko <bernardo.mesko@ip5tecnologia.com.br>
2024-08-05 14:30:15 -0300

    Install External Secrets Operator in AKS

---
+++ kubernetes-infra/main.tf
@@ -0,0 +1,45 @@
+provider "helm" {
+  kubernetes {
+    config_path            = "~/.kube/config"
+    config_context_cluster = "aks-secrets"
+  }
+}
+
+provider "kubectl" {
+  config_path            = "~/.kube/config"
+  config_context_cluster = "aks-secrets"
+}
+
+# Define a variable for the External Secrets operator version
+variable "external_secrets_version" {
+  description = "The version of the External Secrets operator"
+  type        = string
+  default     = "0.10.0"
+}
+
+# # Fetch the CRD manifest from the internet using the version variable
+# data "http" "external_secrets_crds" {
+#   url = "https://raw.githubusercontent.com/external-secrets/external-secrets/v${var.external_secrets_version}/deploy/crds/bundle.yaml"
+# }
+
+# # Apply each resource in the CRD manifest
+# resource "kubectl_manifest" "external_secrets_crd" {
+#   for_each  = data.http.external_secrets_crds
+#   yaml_body = each.value
+# }
+
+# nstall the Helm chart for external-secrets
+resource "helm_release" "external_secrets" {
+  name             = "external-secrets"
+  namespace        = "external-secrets"
+  create_namespace = true
+
+  repository = "https://charts.external-secrets.io"
+  chart      = "external-secrets"
+  version    = var.external_secrets_version
+
+  set {
+    name  = "installCRDs"
+    value = "true" # couldnt bother to try to handle the crd files
+  }
+}
